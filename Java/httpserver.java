8dd0cb134699d20ceffb4f0dc0181755a31db744
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-04-06 16:08:16 -0400

    Create httpserver.java

---
+++ Java/httpserver.java
@@ -0,0 +1,18 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.io.IOException;
+
+public class httpserver{
+    public static void main(String args[]) throws IOException{
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while (true){
+            try (Socket socket = server.accept()){
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+            }
+        }
+    }
+}
